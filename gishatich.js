5563df718e9177cbb44a34cd5dfba5bbca26b3cd
yourusername <your@email.com>
2018-12-03 14:30:30 +0400

    projecti karchacum

--- gishatich.js
+++ gishatich.js
@@ -1,19 +1,8 @@
-class Gishatich {
+class Gishatich extends LivingCreature {
     constructor(x, y, index) {
-        this.x = x;
-        this.y = y;
-        this.index = index;
+       super(x,y,index);
         this.multiply = 0;
         this.energy = 15;
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]];
     }
     getNewDirections() {
         this.directions = [
@@ -28,19 +17,7 @@ class Gishatich {
     }
     chooseCell(character) {
         this.getNewDirections()
-
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-        return found;
+        return super.chooseCell(character);
     }
 
     mult() {
--- grass.js
+++ grass.js
@@ -1,17 +1,4 @@
 class Grass extends LivingCreature {
-    constructor(x, y, index) {
-       super(x, y, index);
-    }
-    getNewDirections() {
-        [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1];
-    }
     mult() {
         var empty = random(this.chooseCell(0));
         if (empty) {
